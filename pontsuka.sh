78597889db414b7766f9308a007291a6dc546215
EC2 Default User <ec2-user@ip-172-31-29-22.ap-northeast-1.compute.internal>
2017-01-04 14:48:33 +0000

    cron job failed cat /var/log/cron -> permission deneid
    add
    	cd $workdir
    
    mod
    	file size check

--- pontsuka.sh
+++ pontsuka.sh
@@ -6,9 +6,19 @@ URL_posfix=".mp3"
 URL=$URL_prefix$PONTSUKA_DATE$URL_posfix
 FILE_NAME=bump-$PONTSUKA_DATE$URL_posfix
 
+cd $WORK_DIR
+echo $PONTSUKA_DATE
 echo wget $URL
 wget -O $WORK_DIR$FILE_NAME $URL
 case $? in
-    "0" ) /home/ec2-user/Dropbox-Uploader/dropbox_uploader.sh upload $WORK_DIR$FILE_NAME $FILE_NAME ;;
+    "0" ) echo "wget is successful" ;;
     "8" ) echo "[Error] 404" ;;
 esac
+if [ -s $WORK_DIR$FILE_NAME ]; then
+  # 1バイトでも中身があれば何もしない
+  /home/ec2-user/Dropbox-Uploader/dropbox_uploader.sh upload $WORK_DIR$FILE_NAME $FILE_NAME
+else
+  # 0バイトだったら消す
+  rm $WORK_DIR$FILE_NAME
+  echo "mp3 file is 0 byte deleted"
+fi
