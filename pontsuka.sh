c00220a7d1c8b07bc1dd2d8a4a3d45a88ea41da1
asapoka <asapoka@outlook.com>
2017-01-05 15:41:43 +0000

    add same file check

--- pontsuka.sh
+++ pontsuka.sh
@@ -5,7 +5,8 @@ URL_prefix="http://vod.bayfm.jp/VODFILES/artist/bump-"
 URL_posfix=".mp3"
 URL=$URL_prefix$PONTSUKA_DATE$URL_posfix
 FILE_NAME=bump-$PONTSUKA_DATE$URL_posfix
-
+TEMP_FILE_NAME=$(mktemp)
+RET_FILE=$(mktemp)
 
 cd $WORK_DIR
 
@@ -15,18 +16,27 @@ esac
 
 echo $PONTSUKA_DATE
 echo wget $URL
-wget -O $WORK_DIR$FILE_NAME $URL
+wget -q -O $TEMP_FILE_NAME $URL
 
 case $? in
     "0" ) echo "wget is successful" ;;
     "8" ) echo "[Error] 404" ;;
 esac
 
-if [ -s $WORK_DIR$FILE_NAME ]; then
+MD5=$(md5sum $TEMP_FILE_NAME)
+
+echo $MD5
+md5sum * > $RET_FILE
+
+grep $MD5 $RET_FILE
+
+if [ $? = 1 ]; then
   # 1バイトでも中身があれば何もしない
+  mv $TEMP_FILE_NAME $WORK_DIR$FILE_NAME
   /home/ec2-user/Dropbox-Uploader/dropbox_uploader.sh upload $WORK_DIR$FILE_NAME $FILE_NAME
 else
   # 0バイトだったら消す
-  rm $WORK_DIR$FILE_NAME
-  echo "mp3 file is 0 byte deleted"
+  rm $TEMP_FILE_NAME
+  echo "the same file already exists"
 fi
+rm $RET_FILE
