fd036e2b0353d7c2e9093686f67c9a47192e8439
asapoka <asapoka@outlook.com>
2017-01-05 22:38:43 +0900

    work dirを直書きしててのを修正 なければ作成するようにする予定

--- pontsuka.sh
+++ pontsuka.sh
@@ -1,19 +1,23 @@
 youbi=$(date +%w)
-WORK_DIR=/home/ec2-user/pontsuka/
+WORK_DIR=~/pontsuka/
 PONTSUKA_DATE=$(date -d "$youbi days ago" +%Y%m%d)
 URL_prefix="http://vod.bayfm.jp/VODFILES/artist/bump-"
 URL_posfix=".mp3"
 URL=$URL_prefix$PONTSUKA_DATE$URL_posfix
 FILE_NAME=bump-$PONTSUKA_DATE$URL_posfix
 
+
 cd $WORK_DIR
+
 echo $PONTSUKA_DATE
 echo wget $URL
 wget -O $WORK_DIR$FILE_NAME $URL
+
 case $? in
     "0" ) echo "wget is successful" ;;
     "8" ) echo "[Error] 404" ;;
 esac
+
 if [ -s $WORK_DIR$FILE_NAME ]; then
   # 1バイトでも中身があれば何もしない
   /home/ec2-user/Dropbox-Uploader/dropbox_uploader.sh upload $WORK_DIR$FILE_NAME $FILE_NAME
