8fed92b7142f51d121ec2c90dc6876d74ce30734
asapoka <asapoka@outlook.com>
2021-09-20 23:03:29 +0900

    exclude link .DS_Store

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ do
   # 無視したいファイルやディレクトリはこんな風に追加してね
   [[ ${f} = ".git" ]] && continue
   [[ ${f} = ".gitignore" ]] && continue
+  [[ ${f} = ".DS_Store" ]] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
