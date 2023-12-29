fb8cce076326ed893957c302419e18c242d90aca
asapoka <asapoka@outlook.com>
2023-12-29 21:14:46 +0900

    add function install_profile

--- PowershellScript/install.ps1
+++ PowershellScript/install.ps1
@@ -1,3 +1,19 @@
-New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
-New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
-New-Item -Path $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
+function install_profile {
+    param (
+        $path
+    )
+    if (Test-Path $path) {
+        Write-Host "already exist "  (Get-Item $path).FullName 
+    } else {
+        New-Item -Path $path -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
+    }
+}
+
+$pwsh = Join-Path $env:USERPROFILE \Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+$vscode = Join-Path $env:USERPROFILE \Documents\PowerShell\Microsoft.VSCode_profile.ps1
+$ps = Join-Path  $env:USERPROFILE \Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
+
+install_profile($pwsh)
+install_profile($vscode)
+install_profile($ps)
+
