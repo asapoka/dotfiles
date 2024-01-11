520b5b91ea6dd0486f36041373bbf4edc3bb6d06
asapoka <asapoka@outlook.com>
2024-01-11 23:58:27 +0900

    set alias eza

--- PowershellScript/Microsoft.PowerShell_profile.ps1
+++ PowershellScript/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # New-Item -Type SymbolicLink $PROFILE -Value Microsoft.PowerShell_profile.ps1
 Set-Alias touch New-Item
+Set-Alias ls eza
 # Ctrl + f で入力補完を確定、次の候補を選択
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptNextSuggestionWord
@@ -24,3 +25,16 @@ function dot {
 }
 
 Invoke-Expression (&starship init powershell)
+function ls {
+    eza --icons --git
+}  
+function ll {
+    eza --icons --git -l
+}  
+function lt {
+    eza -T -L 3 -a -I "node_modules|.git|.cache" --icons
+}
+function ltl {
+    eza -T -L 3 -a -I "node_modules|.git|.cache" -l --icons
+}
+    
