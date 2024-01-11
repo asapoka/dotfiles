b6f29573f8efeccf632c864890f97393b8129743
asapoka <asapoka@outlook.com>
2024-01-11 23:46:52 +0900

    remove oh my posh add starship

--- PowershellScript/Microsoft.PowerShell_profile.ps1
+++ PowershellScript/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,5 @@
 # New-Item -Type SymbolicLink $PROFILE -Value Microsoft.PowerShell_profile.ps1
 Set-Alias touch New-Item
-oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/hul10.omp.json' | Invoke-Expression
 # Ctrl + f で入力補完を確定、次の候補を選択
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptNextSuggestionWord
@@ -24,3 +22,5 @@ function source {
 function dot {
     code $env:USERPROFILE/dotfiles
 }
+
+Invoke-Expression (&starship init powershell)
