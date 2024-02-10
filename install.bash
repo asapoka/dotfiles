8a378917d537e9fbac41a7c933ccd56f1cd3c2d9
asapoka <asapoka@outlook.com>
2024-02-11 01:14:02 +0900

    並び替え

--- install.bash
+++ install.bash
@@ -48,15 +48,6 @@ if ! command -v sheldon >/dev/null 2>&1; then
   echo -e "\e[36mInstalled sheldon\e[m\n"
 fi
 
-# starship
-if [ ! -d ${HOME}/.config ]; then
-  # .configが存在しない場合は作成
-  mkdir ${HOME}/.config
-  ln -snfv ${DOT_DIRECTORY}/starship.toml ${HOME}/.config/starship.toml
-else
-  ln -snfv ${DOT_DIRECTORY}/starship.toml ${HOME}/.config/starship.toml
-fi
-
 if ! command -v starship >/dev/null 2>&1; then
   echo -e "\e[36mInstalling... starship\e[m\n"
   brew install starship
@@ -74,4 +65,14 @@ if ! command -v fzf >/dev/null 2>&1; then
   brew install fzf
   echo -e "\e[36mInstalled fzf\e[m\n"
 fi
+
+# starship
+if [ ! -d ${HOME}/.config ]; then
+  # .configが存在しない場合は作成
+  mkdir ${HOME}/.config
+  ln -snfv ${DOT_DIRECTORY}/starship.toml ${HOME}/.config/starship.toml
+else
+  ln -snfv ${DOT_DIRECTORY}/starship.toml ${HOME}/.config/starship.toml
+fi
+
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
