6c100481998d8bc67e2910819a21ebb12ef478f9
asapoka <asapoka@outlook.com>
2025-04-19 23:52:56 +0900

    nvim設定を更新し、日本語ヘルプと日本語ドキュメントプラグインを有効化

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -31,7 +31,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'jj', '<Esc>') -- jjでインサートモードを抜ける
 vim.keymap.set('n', 'J', '10j')
 vim.keymap.set('n', 'K', '10k')
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
