958ea8b2e06255a7711877b24eac54426bdd15d3
asapoka <asapoka@outlook.com>
2025-06-28 08:13:15 +0900

    colors.bashを新規作成し、install.bashでの色定義とメッセージ関数を移動

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,35 +1,10 @@
 #!/usr/bin/env bash
 
 DOT_DIR="$HOME/dotfiles"
-COLOR_GRAY="\033[1;38;5;243m"
-COLOR_BLUE="\033[1;34m"
-COLOR_GREEN="\033[1;32m"
-COLOR_RED="\033[1;31m"
-COLOR_PURPLE="\033[1;35m"
-COLOR_YELLOW="\033[1;33m"
-COLOR_NONE="\033[0m"
+
 set -euo pipefail
-title() {
-  echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
-  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
-}
 
-error() {
-  echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
-  exit 1
-}
-
-warning() {
-  echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
-}
-
-info() {
-  echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
-}
-
-success() {
-  echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
-}
+source $DOT_DIR/lib/colors.bash
 
 # install command if command is not installed
 install_command() {
@@ -111,7 +86,7 @@ if [ ! -d ${HOME}/.config/alacritty ]; then
   mkdir -p ${HOME}/.config/alacritty
   ln -snfv ${DOT_DIR}/configs/terminal/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
 else
-  ln -snfv ${DOT_DIR}/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
+  ln -snfv ${DOT_DIR}/configs/terminal/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
 fi
 
 # nvim
