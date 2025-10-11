d616cd32606338a6981a1173bc2346e00017cf09
asapoka <asapoka@outlook.com>
2025-10-11 15:08:29 +0900

    関数定義の形式を統一し、各関数に「function」キーワードを追加

--- config/zsh/aliases/function.zsh
+++ config/zsh/aliases/function.zsh
@@ -1,10 +1,10 @@
-fman() {
+function fman() {
     man -k . | fzf --prompt='Man> ' | awk '{print $1}' | xargs -r man
 }
 # Install (one or multiple) selected application(s)
 # using "brew search" as source input
 # mnemonic [B]rew [I]nstall [P]ackage
-bip() {
+function bip() {
   local inst=$(brew search "$@" | fzf -m)
 
   if [[ $inst ]]; then
@@ -14,7 +14,7 @@ bip() {
 }
 # Update (one or multiple) selected application(s)
 # mnemonic [B]rew [U]pdate [P]ackage
-bup() {
+function bup() {
   local upd=$(brew leaves | fzf -m)
 
   if [[ $upd ]]; then
@@ -24,7 +24,7 @@ bup() {
 }
 # Delete (one or multiple) selected application(s)
 # mnemonic [B]rew [C]lean [P]ackage (e.g. uninstall)
-bcp() {
+function bcp() {
   local uninst=$(brew leaves | fzf -m)
 
   if [[ $uninst ]]; then
@@ -36,7 +36,7 @@ bcp() {
 # Install or open the webpage for the selected application
 # using brew cask search as input source
 # and display a info quickview window for the currently marked application
-install() {
+function install() {
     local token
     token=$(brew search --casks "$1" | fzf-tmux --query="$1" +m --preview 'brew info {}')
 
@@ -56,7 +56,7 @@ install() {
 # Uninstall or open the webpage for the selected application
 # using brew list as input source (all brew cask installed applications)
 # and display a info quickview window for the currently marked application
-uninstall() {
+function uninstall() {
     local token
     token=$(brew list --casks | fzf-tmux --query="$1" +m --preview 'brew info {}')
 
